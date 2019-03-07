fe854ce8fe4c75b07daa0009ec4787c78187bf82
TK <zeropercent@hotmail.com>
2019-03-07 20:33:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// add solution here
-function theBeatlesPlay(musician, instrument){
-  var musician_instrument = []
-  for( var i = 0, i < musician.length, i++ ){
-    musician_instrument[i] = musician[i] + " plays " + instrument[i]
-  }
-  return musician_instrument
-}
+// // add solution here
+// function theBeatlesPlay(musician, instrument){
+//   var musician_instrument = []
+//   for( var i = 0, i < musician.length, i++ ){
+//     musician_instrument[i] = musician[i] + " plays " + instrument[i]
+//   }
+//   return musician_instrument
+// }
 
 const facts = [
   "He was the last Beatle to learn to drive",
