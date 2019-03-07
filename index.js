ef9012373b21596707b36c10d2e37712686975bc
TK <zeropercent@hotmail.com>
2019-03-07 21:08:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
-  var musician_instrument = [];
+  var musician_instrument = []
   for( var i = 0; i < musicians.length; i++ ){
     // musician_instrument[i] = musician[i] + " plays " + instrument[i]
-    musician_instrument.push(musicians[i] + " plays " + instruments[i]);
+    musician_instrument.push(musicians[i] + " plays " + instruments[i])
   }
-  return musician_instrument;
+  return musician_instrument
 }
 
 // const facts = [
