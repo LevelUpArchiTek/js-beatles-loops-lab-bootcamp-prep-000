048743a6d75a914c53daeaeda98865e182b64613
TK <zeropercent@hotmail.com>
2019-03-07 21:22:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,22 @@ function theBeatlesPlay(musicians, instruments){
 //   "He hated the sound of his own voice"
 // ];
 
+function johnLennonFacts(facts){
+  facts = [
+    "He was the last Beatle to learn to drive",
+    "He was never a vegetarian",
+    "He was a choir boy and boy scout",
+    "He hated the sound of his own voice"
+  ];
+  var newFacts = [];
+  var i = 0;
+  While (i<facts.length){
+    // facts[i] = fact[i] + "!!!"
+    newFacts.push(facts[i] + "!!!");
+    i++;
+  }
+  return newFacts;
+}
 
 function iLoveTheBeatles(number){
   var x = []
