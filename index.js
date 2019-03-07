e28f89fd33ddfc81ec32a7f60d7f1635c436b1f8
TK <zeropercent@hotmail.com>
2019-03-07 20:19:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musician, instrument){
   var musician_instrument = []
   for( var i = 0, i < musician.length, i++){
-    musician_instrument[i] = musician + " plays " + instrument
+    musician_instrument[i] = musician[i] + " plays " + instrument[i]
   }
   return musician_instrument
 }
