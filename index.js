a706e60c4daa63ece0ebff57b0a385793f481df4
TK <zeropercent@hotmail.com>
2019-03-07 21:35:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
-function johnLennonFacts() {
-    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice", "foo", "bar"];
-    var newFacts = [];
-    var i = 0;
-    while (i < facts.length) {
-        newFacts.push(facts[i] +"!!!");
-        i++;
-    }
-    return newFacts;
+// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var musician_instrument = []
+  for( var i = 0; i < musicians.length; i++ ){
+    // musician_instrument[i] = musician[i] + " plays " + instrument[i]
+    musician_instrument[i] = musicians[i] + " plays " + instruments[i]
+  }
+  return musician_instrument
+}
+
+// const facts = [
+//   "He was the last Beatle to learn to drive",
+//   "He was never a vegetarian",
+//   "He was a choir boy and boy scout",
+//   "He hated the sound of his own voice"
+// ];
+function johnLennonFacts(){
+  const facts = [
+    "He was the last Beatle to learn to drive",
+    "He was never a vegetarian",
+    "He was a choir boy and boy scout",
+    "He hated the sound of his own voice"
+  ]
+  var newFacts = []
+  var i = 0
+  While (i<facts.length){
+    // facts[i] = fact[i] + "!!!"
+    newFacts.push(facts[i] + "!!!")
+    i++
+  }
+  return newFacts
+}
+
+function iLoveTheBeatles(number){
+  var x = []
+  do {
+    // x[number] = "I love the Beatles!"
+    x.push("I love the Beatles!")
+    number++
+  } while (number<15)
+  return x
 }
